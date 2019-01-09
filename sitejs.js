a954fdfc941b7756539d8676c586b27f51e90e9b
Alex Palumbo <palu3492@stthomas.edu>
2019-01-08 22:04:48 -0600

    Updates

--- sitejs.js
+++ sitejs.js
@@ -1,35 +1,38 @@
+var canvas;
+var grid;
 
-var canvas = new fabric.Canvas('stage-canvas');
-var grid = 35;
-var w1 = screen.height;
-fabric.Object.prototype.hasControls = false; //objects on canvas can't be resized/rotated
+document.addEventListener("DOMContentLoaded", function () {
+    main();
+});
 
+function main() {
+    canvas = new fabric.Canvas('stage-canvas');
+    fabric.Object.prototype.hasControls = false;  //objects on canvas can't be resized/rotated
+    addGrid();
+    grid = 35;
+
+    canvas.on('object:moving', function (options) {
+        options.target.set({
+            left: Math.round(options.target.left / grid) * grid,
+            top: Math.round(options.target.top / grid) * grid
+        });
+    });
+}
 
 // create grid
-
-for (var i = 0; i < (2000 / grid); i++) {
-  canvas.add(new fabric.Line([ i * grid, 0, i * grid, 2000], { stroke: '#f7f7f7', selectable: false }));
-  canvas.add(new fabric.Line([ 0, i * grid, 2000, i * grid], { stroke: '#f7f7f7', selectable: false}))
+function addGrid() {
+    for (var i = 0; i < (2000 / grid); i++) {
+        canvas.add(new fabric.Line([i * grid, 0, i * grid, 2000], {stroke: '#f7f7f7', selectable: false}));
+        canvas.add(new fabric.Line([0, i * grid, 2000, i * grid], {stroke: '#f7f7f7', selectable: false}))
+    }
 }
 
 // add objects
-
 // snap to grid
 
-canvas.on('object:moving', function(options) { 
-  options.target.set({
-    left: Math.round(options.target.left / grid) * grid,
-    top: Math.round(options.target.top / grid) * grid
-  });
-});
-
-/* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- */
 
 
-/* ------------------------------------- */
 
-
-/* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- */
 function handleDragStart(e) {
     [].forEach.call(images, function (img) {
         img.classList.remove('img_dragging');
@@ -77,25 +80,24 @@ function handleDrop(e) {
         top: e.layerY
     });
     canvas.add(newImage);
-	
-	
 
-	if(img.className!="my-image1 img_dragging canvas-img"){ //makes sure it should have text
-	var newText = new fabric.IText('#', {					//creates text
-		left: e.layerX + 6,
-		top: e.layerY + 3,
-		fontFamily: 'Helvetica',
-		fontSize: 30,
-		fill: 'white',
-})
-canvas.add(newText)
-canvas.bringToFront(newText)
-newText.enterEditing();
-newText.selectAll();
-}
-var groupp = new fabric.Group([ newImage, newText,]);
 
-canvas.add(groupp);
+    if (img.className != "my-image1 img_dragging canvas-img") { //makes sure it should have text
+        var newText = new fabric.IText('#', {					//creates text
+            left: e.layerX + 6,
+            top: e.layerY + 3,
+            fontFamily: 'Helvetica',
+            fontSize: 30,
+            fill: 'white',
+        })
+        canvas.add(newText)
+        canvas.bringToFront(newText)
+        newText.enterEditing();
+        newText.selectAll();
+    }
+    var groupp = new fabric.Group([newImage, newText,]);
+
+    canvas.add(groupp);
 
     return false;
 }
@@ -126,60 +128,61 @@ if (window.FileReader) {
     // Replace with a fallback to a library solution.
     alert("This browser doesn't support the HTML5 Drag and Drop API.");
 }
-/* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- */
-function deleteObjects(){
-	var activeObject = canvas.getActiveObject(),
-    activeGroup = canvas.getActiveGroup();
+
+function deleteObjects() {
+    var activeObject = canvas.getActiveObject(),
+        activeGroup = canvas.getActiveGroup();
     if (activeObject) {
-            canvas.remove(activeObject);
-        
+        canvas.remove(activeObject);
+
     }
     else if (activeGroup) {
-    
-            var objectsInGroup = activeGroup.getObjects();
-            canvas.discardActiveGroup();
-            objectsInGroup.forEach(function(object) {
+        var objectsInGroup = activeGroup.getObjects();
+        canvas.discardActiveGroup();
+        objectsInGroup.forEach(function (object) {
             canvas.remove(object);
-            });
-        
+        });
     }
 }
-$("#delete").click(function(){
+
+$("#delete").click(function () {
     deleteObjects();
 });
 
-/* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- */
 
-function outNotepad(){
-var notepadID = document.getElementById('notepad');
-var inbuttonID = document.getElementById('buttonnotepadin');
-notepadID.style.right = "10px";
-inbuttonID.style.right = "385px";
+function outNotepad() {
+    var notepadID = document.getElementById('notepad');
+    var inbuttonID = document.getElementById('buttonnotepadin');
+    notepadID.style.right = "10px";
+    inbuttonID.style.right = "385px";
 }
-function inNotepad(){
-var notepadID = document.getElementById('notepad');
-var inbuttonID = document.getElementById('buttonnotepadin');
-notepadID.style.right = "-800px";
-inbuttonID.style.right = "-50px";
+
+function inNotepad() {
+    var notepadID = document.getElementById('notepad');
+    var inbuttonID = document.getElementById('buttonnotepadin');
+    notepadID.style.right = "-800px";
+    inbuttonID.style.right = "-50px";
 }
-/* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- *//* ------------------------------------- */
+
+
 function clearCanvas() {
-canvas.clear();
-// creates grid
-for (var i = 0; i < (2000 / grid); i++) {
-  canvas.add(new fabric.Line([ i * grid, 0, i * grid, 2000], { stroke: '#f7f7f7', selectable: false }));
-  canvas.add(new fabric.Line([ 0, i * grid, 2000, i * grid], { stroke: '#f7f7f7', selectable: false}))
+    canvas.clear();
+    // creates grid
+    for (var i = 0; i < (2000 / grid); i++) {
+        canvas.add(new fabric.Line([i * grid, 0, i * grid, 2000], {stroke: '#f7f7f7', selectable: false}));
+        canvas.add(new fabric.Line([0, i * grid, 2000, i * grid], {stroke: '#f7f7f7', selectable: false}))
+    }
+
+    // snap to grid
+
+    canvas.on('object:moving', function (options) {
+        options.target.set({
+            left: Math.round(options.target.left / grid) * grid,
+            top: Math.round(options.target.top / grid) * grid
+        });
+    });
 }
 
-// snap to grid
-
-canvas.on('object:moving', function(options) { 
-  options.target.set({
-    left: Math.round(options.target.left / grid) * grid,
-    top: Math.round(options.target.top / grid) * grid
-  });
-});
-}
 function exportSVG() {
 
     var svg = canvas.toSVG();
@@ -189,40 +192,41 @@ function exportSVG() {
 }
 
 
-function ChangeText(){ //change text function
-//	remove group
-var act = canvas.getActiveObject(); //get object slected
-var items = act._objects;			//gets all items in group
-act._restoreObjectsState();			//puts them to original state
-canvas.remove(act);					//removes them
-for(var i = 0; i < items.length; i++) {
-  canvas.add(items[i]);				//adds items back and enters editing
-}
-canvas.renderAll();
-if (items.length>1){
-items[1].enterEditing();
-items[1].selectAll();
-}
-var groupp2 = new fabric.Group(items);
+function ChangeText() { //change text function
+    //	remove group
+    var act = canvas.getActiveObject(); //get object slected
+    var items = act._objects;			//gets all items in group
+    act._restoreObjectsState();			//puts them to original state
+    canvas.remove(act);					//removes them
+    for (var i = 0; i < items.length; i++) {
+        canvas.add(items[i]);				//adds items back and enters editing
+    }
+    canvas.renderAll();
+    if (items.length > 1) {
+        items[1].enterEditing();
+        items[1].selectAll();
+    }
+    var groupp2 = new fabric.Group(items);
 
-canvas.add(groupp2);
-};
+    canvas.add(groupp2);
+}
+
 window.fabric.util.addListener(canvas.upperCanvasEl, 'dblclick', function (event, self) {
-  ChangeText();
+    ChangeText();
 });
 
 canvas.observe('mouse:over', function (e) { 	//so grid wont have cursor
-            if (e.target.get('type') == 'line') {
+    if (e.target.get('type') == 'line') {
 
-                e.target.hoverCursor = 'default';
-            }
+        e.target.hoverCursor = 'default';
+    }
 
-        });
-$(document).keydown(function(e) {
-    if(e.keyCode == 46) {
-        deleteObjects();
-    }      
 });
-$("#dupB").click(function(){
+$(document).keydown(function (e) {
+    if (e.keyCode == 46) {
+        deleteObjects();
+    }
+});
+$("#dupB").click(function () {
     cloneObj();
 });
