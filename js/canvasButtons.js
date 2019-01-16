2bc15cea7697f3399b5744709caaa17c9be9a212
Alex Palumbo <palu3492@stthomas.edu>
2019-01-16 09:44:31 -0600

    Updates

--- js/canvasButtons.js
+++ js/canvasButtons.js
@@ -93,17 +93,38 @@ function clearCanvas() {
 }
 
 function saveImg(){
-    // window.open(fabricCanvas.toDataURL('png'));
+    var pdfDoc = new jsPDF({orientation: 'landscape', unit: 'in'});
+
+    var title = $('#title-input').val();
+    if(!title){
+        title = "Stage Diagram";
+    }
+    pdfDoc.setFontSize(25);
+    pdfDoc.text(0.75, 0.75, title);
+    var notes = $('#notes-area').val();
+    if(notes) {
+        pdfDoc.setFontSize(15);
+        pdfDoc.text(0.75, 6.25, "Notes:");
+        pdfDoc.setFontSize(12);
+        pdfDoc.text(0.75, 6.5, notes, {maxWidth: 10});
+    }
+    //get canvas size and crop image
+    var canvasURL = fabricCanvas.toDataURL('png');
+    pdfDoc.addImage(canvasURL, 'JPEG', 0.75, 1, 8.88, 5);
+    // add stage bottom
+    var image = $('#stage-front')[0];
+    pdfDoc.addImage(image, 'JPEG', 0.75, 4, 8.88, 2);
+    pdfDoc.save(title.replace(" ", "") + 'SD.pdf');
 }
 
 function openNotes(){
     var main = $('#main');
     $('#right')[0].style.display = "initial";
-    main.css("grid-template-columns", "260px auto 260px");
+    main.css("grid-template-columns", "262px auto 260px");
 }
 
 function closeNotes(){
     var main = $('#main');
     $('#right')[0].style.display = "none";
-    main.css("grid-template-columns", "260px auto 0");
+    main.css("grid-template-columns", "262px auto 0");
 }
--- js/canvasObjects.js
+++ js/canvasObjects.js
@@ -19,16 +19,16 @@ document.addEventListener("DOMContentLoaded", function () {
     //     }
     // });
 
-    $('input').keypress(function(e) {
-        var a = [];
-        var k = e.which;
-
-        for (i = 48; i < 58; i++)
-            a.push(i);
-
-        if (!(a.indexOf(k)>=0))
-            e.preventDefault();
-    });
+    // $('input').keypress(function(e) {
+    //     var a = [];
+    //     var k = e.which;
+    //
+    //     for (i = 48; i < 58; i++)
+    //         a.push(i);
+    //
+    //     if (!(a.indexOf(k)>=0))
+    //         e.preventDefault();
+    // });
 });
 
 function editText(){
